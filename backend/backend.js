2f66150ac8038ce27fb857ce4372d38c2d63f4f2
arkady likovizki <arkados2@gmail.com>
2021-11-13 19:49:31 +0200

    cors fix

--- backend/backend.js
+++ backend/backend.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: "*" }));
 
 app.get("/", (req, res) => {
   res.status(200).send("hello !");
