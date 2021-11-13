e51ce505c20035b7530ec1fc0941d503f0e4855f
arkady likovizki <arkados2@gmail.com>
2021-11-13 19:53:37 +0200

    cors fix 2

--- backend/backend.js
+++ backend/backend.js
@@ -2,7 +2,12 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors({ credentials: true, origin: "*" }));
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://ec2-18-234-233-0.compute-1.amazonaws.com:3000/",
+  })
+);
 
 app.get("/", (req, res) => {
   res.status(200).send("hello !");
