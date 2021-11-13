b0d5bfe9dfbd0b3af00484613d982de208ebf543
arkady likovizki <arkados2@gmail.com>
2021-11-13 20:45:21 +0200

    *

--- backend/backend.js
+++ backend/backend.js
@@ -7,10 +7,7 @@ app.use(cors());
 // Add headers before the routes are defined
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "http://ec2-18-234-233-0.compute-1.amazonaws.com:3000"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
   // Request methods you wish to allow
   res.setHeader(
